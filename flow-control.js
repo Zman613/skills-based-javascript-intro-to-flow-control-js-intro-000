b9df8622e709b43be63d67507d90fb6c07151ad3
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-07-11 02:21:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,37 @@
 function basicTeenager(age) {
-
+  if (>=13 && <=19) {
+    //If between the ages of 13 and 19
+    return "You are a teenager!"
+    // Then print "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (>=13 && <=19) {
+    //If between the ages of 13 and 19
+    return "You are a teenager!"
+    //Then print "You are a teenager!"
+  } else {
+    //If above is false
+    return "You are not a teenager!"
+    // Then print "You are not a teenager!"
+  }
 }
 
 function ageChecker(age) {
-
+  if (>=13 && <=19) {
+    //If between the ages of 13 and 19
+    return "You are a teenager"
+    //Then print "You are a teenager!"
+  } else if (<=12) {
+      //If the age is 12 and under
+      return "You are a kid!"
+      //Then print "You are a kid!"
+  } else {
+      //If above is both false
+      return "You are a grownup!"
+      //Then print "You are a grownup!"
+  }
 }
 
 function ternaryTeenager(age) {
